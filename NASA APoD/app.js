b17b0750044bb6330d6dfde73505315583eecd55
Pankaj Kumar Sahu <pankajsahuhero@gmail.com>
2020-08-22 17:12:30 +0530

    Add files via upload

---
+++ NASA APoD/app.js
@@ -0,0 +1,33 @@
+window.addEventListener("load",function(){
+    const date = document.querySelector(".date");
+    const nasaImg = document.querySelector(".nasa-img");
+    const nasaVid = document.querySelector(".nasa-vid");
+    const imgTitle = document.querySelector(".img-title");
+    const imgDesc = document.querySelector(".img-desc");
+ 
+    
+ const api= "https://api.nasa.gov/planetary/apod?api_key=jIB7iMgD1MxCrXhFuH6eBpayJhvJLdd3JuRvEYAf" ; 
+
+ fetch(api)
+    .then(function(response){
+       return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+
+        date.textContent = data.date;
+        
+        imgTitle.textContent = data.title;
+        imgDesc.textContent = data.explanation;
+
+        if(data.media_type == "video"){
+            nasaVid.src = data.url ;
+            nasaImg.style.display = 'none';
+        }
+        else{
+            nasaImg.src = data.url ;
+            nasaVid.style.display = 'none';
+        }
+    });
+
+});
